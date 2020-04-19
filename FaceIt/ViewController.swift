71a15d39b74570bd861b645a17293e3b28d068c9
jimmy <kji0205@gmail.com>
2020-04-19 20:09:20 +0900

    Initial Commit

---
+++ FaceIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by jimmy on 2020/04/19.
+//  Copyright © 2020 Jimmy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
